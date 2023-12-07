06db3b9b7dce4897c54d9b8d3be4e12be4ff3f9d
joaopedmarques <55136718+joaopedmarques@users.noreply.github.com>
2023-12-07 15:59:52 +0000

    pre-interrupt

--- blinky/blinky.c
+++ blinky/blinky.c
@@ -1,27 +1,4 @@
-//*****************************************************************************
-//
-// blinky.c - Simple example to blink the on-board LED.
-//
-// Copyright (c) 2012-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 2.1.4.178 of the EK-TM4C123GXL Firmware Package.
-//
-//*****************************************************************************
-
+//Includes
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
@@ -31,21 +8,7 @@
 #include "inc/hw_types.h"
 #include "LCD.h"
 
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>Blinky (blinky)</h1>
-//!
-//! A very simple example that blinks the on-board LED using direct register
-//! access.
-//
-//*****************************************************************************
 
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
 #ifdef DEBUG
 void
 __error__(char *pcFilename, uint32_t ui32Line)
@@ -54,11 +17,7 @@ __error__(char *pcFilename, uint32_t ui32Line)
 }
 #endif
 
-//*****************************************************************************
-//
-// Blink the on-board LED.
-//
-//*****************************************************************************
+
 int
 main(void)
 {
@@ -72,22 +31,34 @@ main(void)
        //
        // Enable and wait for the port to be ready for access
        //
-       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); //lcd
+       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //Leds
+       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB); //Keypad X
+       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC); //Keypad y
 
-       GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, D4 | D5 | D6 | D7 | EN | RS);
-       //GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, EN | RS); Estao na port A
 
-       //GPIO_PIN_WRITE
 
     volatile uint32_t ui32Loop;
 
     // Enable the GPIO port that is used for the on-board LED.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     // Check if the peripheral access is enabled.
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))
+    {
+    }
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
     {
     }
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB))
+    {
+    }
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC))
+    {
+    }
+
+
+    //enable port B interrupt handler keypad input
+    GPIOIntRegister(GPIO_PORTB_BASE, PortBIntHandler);
 
     //
     // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
@@ -96,20 +67,38 @@ main(void)
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);//Green LED
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);//Blue LED
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);//Red LED
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, D4 | D5 | D6 | D7 | EN | RS); //pins LCD como output
+    GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);   //definir pinos keypad Y como output    PB4,5,6,7
+    GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);    //definir pinos keypad X como input     PC0,1,2,3
+    //GPIO_PIN_WRITE
+
     Lcd_Init();
+
+    //
+    // Make pin 5 high level triggered interrupts.
+    //
+    GPIOIntTypeSet(GPIO_PORTB_BASE, GPIO_PIN_0, GPIO_HIGH_LEVEL);
+    GPIOIntTypeSet(GPIO_PORTB_BASE, GPIO_PIN_1, GPIO_HIGH_LEVEL);
+    GPIOIntTypeSet(GPIO_PORTB_BASE, GPIO_PIN_2, GPIO_HIGH_LEVEL);
+    GPIOIntTypeSet(GPIO_PORTB_BASE, GPIO_PIN_3, GPIO_HIGH_LEVEL);
+
     //
     // Loop forever.
     //
     while(1)
     {
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4, GPIO_PIN_4); //Todos os pins Y do keypad a 1 para ver se alguma tecla é premida
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, GPIO_PIN_5);
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, GPIO_PIN_6);
+        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, GPIO_PIN_7);
         //
-        // Turn on the LED.
+        // Turn on the LED. (Red)
         //
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0x0);
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
 
         //
-        // Turn on the LED
+        // Write to the LCD
         //
         Lcd_Write_Char('G');
         Lcd_Write_Char('R');
@@ -122,19 +111,14 @@ main(void)
         // Delay for a bit
         //
         SysCtlDelay(20000000);
+
         Lcd_Clear();
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 100000; ui32Loop++)
-        {
-        }
 
         //
-        // Turn off the LED.
+        // Turn on/off the LED.
         //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x0);
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x0); //Red LED
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //Blue LED
 
         //
         // Delay for a bit.
